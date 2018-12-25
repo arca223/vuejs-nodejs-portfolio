176ab6e6fc37100a1a336ad23820dbb766dea0f6
Max Coq <arcanis223@gmail.com>
2018-12-25 06:29:43 +0000

    Token safeguard to avoid double events for slider

--- src/components/jobs/Carousel.vue
+++ src/components/jobs/Carousel.vue
@@ -46,42 +46,60 @@
                 activeTile: 0,
                 out: null,
                 in: null,
+                oldTile: null,
+                sliding: false,
             }
         },
         methods: {
+            // Used for the arrow buttons (Icons) answering to click event
             slideForward: function() {
                 this.out = "right";
                 this.in = "left";
+                this.oldTile = this.activeTile;
                 this.activeTile = (this.activeTile === this.experiences.length - 1) ? 0 : ++this.activeTile;
             },
             slideBack: function() {
                 this.out = "left";
                 this.in = "right";
+                this.oldTile = this.activeTile;
                 this.activeTile = (this.activeTile === 0) ? this.experiences.length - 1 : --this.activeTile;
             },
         },
         watch: {
             activeTile: function() {
-                let visibleComponent = $('.transition.visible');
-                let nextComponent = $(`div.job-tile#${this.activeTile}`);
                 let vm = this;
 
+                // sliding is a safeguard token to avoid double clicks.
+                if (this.sliding) {
+                    this.activeTile = this.oldTile;
+                    return;
+                }
+                this.sliding = true;
+                let visibleComponent = $('.transition.visible');
+                let nextComponent = $(`div.job-tile#${this.activeTile}`);
+
                 visibleComponent.transition(`fade ${this.out}`, function () {
-                    nextComponent.transition(`fade ${vm.in}`);
+                    nextComponent.transition(`fade ${vm.in}`, function () {
+                        vm.sliding = false;
+                    });
                 });
             }
         },
         mounted() {
+            // Used for keyup left and right
             // Those methods need to be defined here to be accessed by mounted()
-            // Note that the sementicUI fade event names (right/left) are special, look at the documentation exemples
+            // Note that the semanticUI fade event names (right/left) are inversed compared to my logic/naming
+            // look at the documentation examples to see the output
             const slideForward = () => {
                 this.out = "right";
                 this.in = "left";
+                this.oldTile = this.activeTile;
                 this.activeTile = (this.activeTile === this.experiences.length-1) ? 0 : ++this.activeTile;
             };
             const slideBack = () => {
                 this.out = "left";
                 this.in = "right";
+                this.oldTile = this.activeTile;
                 this.activeTile = (this.activeTile === 0) ? this.experiences.length-1 : --this.activeTile;
             };
 
